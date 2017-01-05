5a868ae65d207cad682843fc0719d084265055f6
Grzegorz Zapała <GrzegorzZ@JETON.local>
2017-01-05 15:10:10 +0100

    pagination done

--- script2.js
+++ script2.js
@@ -8,29 +8,24 @@ $().ready(function () {
         getData: function () {
             // console.log('GetData - this: ' + self);
             var self = this;
-            var url = "http://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch=metal&srlimit=50&titles=metal&origin=*";
+            var url = "http://en.wikipedia.org/w/api.php?format=json&action=query&prop&list=search&srsearch=metal&srlimit=50&titles=metal&origin=*";
             $.getJSON(url, function (data) {
                 console.log(data);
-                data.query.search.forEach(
-                    function (element, index) {
-                        var obj = {}; // bez tego mamy tylko jeden obiekt
-                        obj.title = element.title;
-                        obj.snippet = element.snippet;
-                        obj.index = 
-                        self.dataJSON.push(obj);
+                var length = data.query.search.length;
+                for(var i = 0; i<length; i++){
+                    if(i % 10 === 0){
+                        var arr = [];
                     }
-                );
+                    var obj = {}; // bez tego mamy tylko jeden obiekt
+                    obj.title = data.query.search[i].title;
+                    obj.snippet = data.query.search[i].snippet;
+                    arr.push(obj);
+                    if(i % 10 === 0){
+                        self.dataJSON.push(arr);
+                    }
+                }
                 console.log(self.dataJSON);
             });
-        },
-        // ?
-        setResults: function () {
-
-
-            // console.log('this: ' + this);
-            // console.log('results: ');
-            // console.log(this.results);
-            var pagesNum = data.query.search.length / 10;
         },
 
         // Random button
@@ -39,11 +34,43 @@ $().ready(function () {
 
         // wyświetl dane
         showData: function () {
-            console.log('Show data function');
-            engine.dataJSON.forEach(function (item) {
-                var result = $('div').html('<p>'+item.title+'</p><p>'+item.snippet+'</p>')
-                $('#results').append(result);
+
+            // pagination
+            var navigation = '<nav aria-label="Page navigation">' + 
+                                '<ul class="pagination">'+
+                                    '<li class="page-item">' +
+                                        '<a class="page-link" href="#" aria-label="Previous">' +
+                                        '<span aria-hidden="true">&laquo;</span>' +
+                                        '<span class="sr-only">Previous</span>' +
+                                        '</a>' +
+                                    '</li>';
+            var numPages = engine.dataJSON.length
+            for(var i = 0; i<numPages; i++){
+                navigation += '<li class="page-item"><a class="page-link" data-pagenum="'+i+'" href="#">'+(i+1)+'</a></li>';
+            }
+            navigation += '<li class="page-item">' +
+                                '<a class="page-link" href="#" aria-label="Next">' +
+                                '<span aria-hidden="true">&raquo;</span>' +
+                                '<span class="sr-only">Next</span>' +
+                                '</a>'
+                            '</li>'
+                        '</ul>'
+                    '</nav>';
+            $('.nav').html(navigation);
+            $('a.page-link').click(function() {
+                var pageNum = $(this).data('pagenum');
+                setPage(pageNum);
             });
+            setPage(0);
+            
+            // inner helper function
+            function setPage(num) {
+                $('#results').html('');
+                engine.dataJSON[num].forEach(function (item) {
+                result = $('<div>').html('<p>'+ item.title +'</p><p>'+item.snippet+'</p>')
+                $('#results').append(result);
+                });
+            }
         }
     }
     $('#show').click(engine.showData);
