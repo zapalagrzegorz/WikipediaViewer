1c8cda0e0d983315ca0ebf677180891e04442a9d
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-06-20 11:49:57 +0200

    refreshing

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$().ready(function () {
-	$('#wiki-search-button').click($.proxy(engine.getData, engine));
-  $('#wiki-search-random').click($.proxy(engine.getRandomData, engine))
-  $(document).bind('keypress',pressed);
+/* globals engine  */
 
-});
-
-function pressed(e)
-{
-    if(e.keyCode === 13)
-    {
-       engine.getData();
+document.addEventListener('DOMContentLoaded', function() {
+  function pressed(e) {
+    if (e.keyCode === 13) {
+      engine.getData();
     }
-}
+  }
+
+  $("#wiki-search-button").on("click", engine.getData.bind(engine));
+  $("#wiki-search-random").on("click", engine.getRandomData.bind(engine));
+
+  document.addEventListener("keypress", pressed);
+});
